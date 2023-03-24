b6614ed99ea2653b6e88240bb65f09a2807cdaef
Ahmet Ali Çetin <ahtctn@gmail.com>
2023-03-24 15:20:23 +0300

    Initial Commit

---
+++ FakeAPIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeAPIApp
+//
+//  Created by Ahmet Ali ÇETİN on 24.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
